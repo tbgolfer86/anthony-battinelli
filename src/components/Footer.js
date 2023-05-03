c65e4a3d056232977d2c19823eb193c6034a24d7
tbgolfer86 <tbj86@outlook.com>
2023-05-03 09:30:10 -0400

    formatted header, footer, and navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import '../styles/Footer.css';
-import gitHub from '../images/logos/GitHub_Logo_White.png'
-import gitHubRoundel from '../images/logos/github-mark-white.png'
-import hackerRank from '../images/logos/HackerRank1.png'
-import linkedIn from '../images/logos/LinkedIn-Blue-72-┬«@2x.png'
+import gitHub from '../images/logos/github-mark-white.png';
+import linkedIn from '../images/logos/LI-In-Bug.png';
+import hackerRank from '../images/logos/hackerrank.png';
 
 function Footer() {
     return (
-        <footer className="section-layout" id="contact-me">
-        <h2>Contact Me</h2>
-        <div className="right-side-col">
-            <ul>
-                <li>203•249•1460</li>
-                <li><a href="mailto:tbj86@outlook.com">tbj86@outlook.com</a></li>
-                <li>
-                    <a target="_blank" href="https://github.com/tbgolfer86" rel="noreferrer"><img src={gitHub} alt="GitHub logo"/></a>
-                    <a target="_blank" href="https://github.com/tbgolfer86" rel="noreferrer"><img src={gitHubRoundel} alt="GitHub logo"/></a>
-                </li>
-                <li><a target="_blank" href="https://www.hackerrank.com/tbgolfer86" rel="noreferrer"><img src={hackerRank} alt="HackerRank logo"/></a></li>
-                <li><a target="_blank" href="https://www.linkedin.com/in/anthony-battinelli" rel="noreferrer"><img src={linkedIn} alt="LinkedIn logo"/></a></li>
-            </ul>
-        </div>
-    </footer> 
+        <footer>
+            <div className='icons'>
+                <a target="_blank" href="https://github.com/tbgolfer86" rel="noreferrer"><img src={gitHub} alt="GitHub logo"/></a>
+                <a target="_blank" href="https://www.linkedin.com/in/anthony-battinelli" rel="noreferrer"><img src={linkedIn} alt="LinkedIn logo"/></a>
+                <a target="_blank" href="https://www.hackerrank.com/tbgolfer86" rel="noreferrer"><img src={hackerRank} alt="HackerRank logo"/></a>
+            </div>
+        </footer> 
     );
   }
   
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,15 @@ import banner from '../images/aurora-1197753_1280.jpg'
 
 function Navigation() {
     return (
-        <div>
-        <ul>
-            <li><a href="#about-me">About Me</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer">Resume</a></li>
-            <li><a href="#contact-me">Contact Me</a></li>
-        </ul>
-        <img src={banner} alt="Northern lights"/>
-        </div>
+        <nav>
+            <div className='navbar'>
+                <a href="#about-me">About Me</a>
+                <a href="#work">Work</a>
+                <a target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer">Resume</a>
+                <a href="#contact-me">Contact Me</a>
+            </div>
+            <img src={banner} alt="Northern lights"/>
+        </nav>
     );
   }
   
