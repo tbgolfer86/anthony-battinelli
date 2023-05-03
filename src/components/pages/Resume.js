d302e5e316b5ae592d7edb12d3a57d192c1a7b4f
tbgolfer86 <tbj86@outlook.com>
2023-05-03 17:07:09 -0400

    added resume link to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../../styles/Resume.css';
 
 export default function Resume() {
   return (
     <div>
-      <h2>Resume</h2>      
+      <h2>Resume</h2>
+      <div>
+        <a class="resume" target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer"></a>
+      </div>   
     </div>
   );
 };
