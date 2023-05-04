91365a9e6d6a8a9902f1fd5b21864e1bd1c10584
tbgolfer86 <tbj86@outlook.com>
2023-05-04 17:34:34 -0400

    added useState to footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from "react";
 import '../styles/Footer.css';
 
 import gitHub from '../images/logos/github-mark-white.png';
@@ -6,11 +7,15 @@ import linkedIn from '../images/logos/LI-In-Bug.png';
 import hackerRank from '../images/logos/hackerrank.png';
 
 export default function Footer() {
+  const [animation, setAnimation] = useState('');
+  const [animation1, setAnimation1] = useState('');
+  const [animation2, setAnimation2] = useState('');
+
   return (
     <footer>
-      <a className='animate__animated animate__bounceIn animate__repeat-3' target="_blank" href="https://github.com/tbgolfer86" rel="noreferrer"><img src={gitHub} alt="GitHub logo" /></a>
-      <a className='animate__animated animate__bounceIn animate__repeat-3' target="_blank" href="https://www.linkedin.com/in/anthony-battinelli" rel="noreferrer"><img src={linkedIn} alt="LinkedIn logo" /></a>
-      <a className='animate__animated animate__bounceIn animate__repeat-3' target="_blank" href="https://www.hackerrank.com/tbgolfer86" rel="noreferrer"><img src={hackerRank} alt="HackerRank logo" /></a>
+      <a className={animation} onClick={() => setAnimation('animate__animated animate__bounce animate__repeat-3')} target="_blank" href="https://github.com/tbgolfer86" rel="noreferrer"><img src={gitHub} alt="GitHub logo" /></a>
+      <a className={animation1} onClick={() => setAnimation1('animate__animated animate__bounce animate__repeat-3')} target="_blank" href="https://www.linkedin.com/in/anthony-battinelli" rel="noreferrer"><img src={linkedIn} alt="LinkedIn logo" /></a>
+      <a className={animation2} onClick={() => setAnimation2('animate__animated animate__bounce animate__repeat-3')} target="_blank" href="https://www.hackerrank.com/tbgolfer86" rel="noreferrer"><img src={hackerRank} alt="HackerRank logo" /></a>
     </footer>
   );
 };
