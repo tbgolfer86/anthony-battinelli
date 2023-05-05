3867cc93409e04ec8c26443fb71cdd9c7ae40ffd
tbgolfer86 <tbj86@outlook.com>
2023-05-04 23:50:01 -0400

    messed around with animations

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,8 +4,8 @@ import '../../styles/Resume.css';
 export default function Resume() {
   return (
     <div>
-      <h2 className='animate__animated animate__fadeIn'>Resume</h2>
-      <div className="animate__animated animate__fadeInRight">
+      <h2 className='animate__animated animate__fadeInDown'>Resume</h2>
+      <div className="animate__animated animate__fadeInBottomRight">
         <a className="resume" target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer"></a>
       </div>   
     </div>
