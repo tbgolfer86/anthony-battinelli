bf39186f5a719705cea58597b08a25d78c5ede3d
tbgolfer86 <tbj86@outlook.com>
2023-05-05 11:37:09 -0400

    edited resume animation

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useState } from "react";
 import '../../styles/Resume.css';
 
 export default function Resume() {
+  const [animation1, setAnimation1] = useState('animate__animated animate__rotateInUpLeft');
+
   return (
     <div>
       <h2 className='animate__animated animate__rotateInUpLeft'>Resume</h2>
-      <div className="animate__animated animate__rotateInUpLeft">
+      <div className={animation1} onMouseEnter={() => setAnimation1('animate__animated animate__headShake animate__repeat-1')} onMouseLeave={() => setAnimation1('')}>
         <a className="resume" target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer"></a>
       </div>   
     </div>
