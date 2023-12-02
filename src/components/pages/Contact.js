6c1f34a29e6ecd59da84fdefccaa8874b9c61e73
tbgolfer86 <tbj86@outlook.com>
2023-12-01 19:58:24 -0500

    fixed contact form functionality

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,109 +1,19 @@
 import React, { useState } from 'react';
 import '../../styles/Contact.css';
 
-// Here we import a helper function that will check if the email is valid
-import { validateEmail } from '../../utils/helpers';
-
 export default function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'name') {
-      setName(inputValue);
-    } else if (inputType === 'email') {
-      setEmail(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !name) {
-      setErrorMessage('Email or name is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    if (!message) {
-      setErrorMessage(
-        `Fill out the message section`
-      );
-      return;
-    }
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setName('');
-    setEmail('');
-    setMessage('');
-
-    fetch("https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628", {
-      method: "POST",
-      headers: { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        message: message
-      })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .then(setErrorMessage('Message sent.'))
-    .catch(error => console.log(error));
-  };
-
   return (
     <div>
       <h2 className='animate__animated animate__fadeInDown'>Contact Me</h2>
-      <form className="form animate__animated animate__fadeInUp">
-        <textarea
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          type="name"
-          placeholder="name"
-          rows="1"
-        />
-        <textarea
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="email"
-          rows="1"
-        />
-        <textarea
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Enter a message..."
-          rows="7"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      <form className='form' id="contactform" action="https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628" method="POST">
+        <textarea name="name" type="text" id="name" placeholder='name' rows='1'></textarea>
+        <textarea name="email" type="email" id="email" placeholder='email' rows='1'></textarea>
+        <textarea name="comment" id="comment" placeholder='Enter a message...' rows='7'></textarea>
+
+        <textarea name="_formsubmit_id" type="text" id='honeyspot'></textarea>
+
+        <input value="Submit" type="submit" id='button'></input>
       </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
     </div>
   );
 };
