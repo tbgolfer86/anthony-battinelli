a33908df9fc59c1dcad809731a1b387de7bc6dbb
tbgolfer86 <tbj86@outlook.com>
2023-12-01 20:08:55 -0500

    added animation to form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <div>
       <h2 className='animate__animated animate__fadeInDown'>Contact Me</h2>
-      <form className='form' id="contactform" action="https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628" method="POST">
+      <form className='form animate__animated animate__fadeInDown' id="contactform" action="https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628" method="POST">
         <textarea name="name" type="text" id="name" placeholder='name' rows='1'></textarea>
         <textarea name="email" type="email" id="email" placeholder='email' rows='1'></textarea>
         <textarea name="comment" id="comment" placeholder='Enter a message...' rows='7'></textarea>
