72c37f5cd9e629175eeab619b5f5c59f842ffd2b
tbgolfer86 <tbj86@outlook.com>
2023-12-07 15:18:38 -0500

    changed hover effect on project screenshots

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
   return (
     <div>
       <h2 className='animate__animated animate__rubberBand'>Resume</h2>
-      <div className={animation1} onMouseEnter={() => setAnimation1('animate__animated animate__headShake animate__repeat-1')} onMouseLeave={() => setAnimation1('')}>
+      <div className={animation1}>
         <a className="resume" target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer"></a>
       </div>   
     </div>
