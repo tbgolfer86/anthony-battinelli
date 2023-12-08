09d9f034fa683031391a4be9ee67698d8aef7387
tbgolfer86 <tbj86@outlook.com>
2023-12-08 15:46:04 -0500

    cleaned up form JSX

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,23 +6,23 @@ export default function Contact() {
     <div className='animate__animated animate__fadeInDown'>
       <h2>Contact Me</h2>
       <div className="form-container">
-        <form className="form" id="contactform" action="https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628" method="POST">
+        <form className='form' id="contactform" action="https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628" method="POST">
           <div className="form-group">
-            <label for="name">Your Name:</label>
+            <label>Your Name:</label>
             <input name="name" id="name" type="text"></input>
           </div>
           <div className="form-group">
-            <label for="email">Your Email:</label>
+            <label>Your Email:</label>
             <input name="email" id="email" type="email"></input>
           </div>
-          <div className="form-group" id="honeyspot">
-            <input name="_formsubmit_id" type="text"></input>
-          </div>
+
+          <input name="_formsubmit_id" id="honeyspot" type="text"></input>
+
           <div className="form-group">
-            <label for="textarea">Send a message:</label>
+            <label>Send a message:</label>
             <textarea cols="50" rows="10" name="comment" id="comment"></textarea>
           </div>
-          <button type="submit" className="form-submit-btn">Submit</button>
+          <input value="Submit" type="submit" className="form-submit-btn"></input> 
         </form>
       </div>
     </div>
