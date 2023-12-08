5172521a176e0377b9f938acd5043cde181da2cd
tbgolfer86 <tbj86@outlook.com>
2023-12-08 18:21:23 -0500

    cleaned up contact form code

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,19 +9,19 @@ export default function Contact() {
         <form className='form' id="contactform" action="https://formsubmit.io/send/4d383f56-1fd7-48f9-b15e-b86eaa5a8628" method="POST">
           <div className="form-group">
             <label>Your Name:</label>
-            <input name="name" id="name" type="text"></input>
+            <input name="name" type="text" id="name"></input>
           </div>
           <div className="form-group">
             <label>Your Email:</label>
-            <input name="email" id="email" type="email"></input>
+            <input name="email" type="email" id="email"></input>
           </div>
-
-          <input name="_formsubmit_id" id="honeyspot" type="text"></input>
-
           <div className="form-group">
             <label>Send a message:</label>
-            <textarea cols="50" rows="10" name="comment" id="comment"></textarea>
+            <textarea name="comment" id="comment" cols="50" rows="10"></textarea>
           </div>
+
+          <input name="_formsubmit_id" type="text" style={{display: "none"}}></input>
+
           <input value="Submit" type="submit" className="form-submit-btn"></input> 
         </form>
       </div>
