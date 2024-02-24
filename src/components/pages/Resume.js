07a2550ba5510ef597457173e241921467bbf151
Tony Battinelli Jr. <tbj86@outlook.com>
2024-02-24 17:30:05 -0500

    updated resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,7 +9,7 @@ export default function Resume() {
     <div>
       <h2 className='animate__animated animate__rubberBand'>Resume</h2>
       <div className={animation1}>
-        <a className="resume" target="_blank" href="https://docs.google.com/document/d/1WKeuCcx_1n9wO6h4XkL0-0THvJLYb3gm5c9wr3qV4eU/edit?usp=sharing" rel="noreferrer"></a>
+        <a className="resume" target="_blank" href="https://1drv.ms/w/s!AnB1yNkSA_167zrbLQgzs2G1e2_F?e=Bfj5Pv" rel="noreferrer"></a>
       </div>   
     </div>
   );
